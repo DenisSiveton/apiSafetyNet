b3efbe84b6b262a72639ebd04ab0ed6658bef6e9
Denis Siveton <64313109+DenisSiveton@users.noreply.github.com>
2021-05-19 00:14:18 +0200

    files from Spring Initializr
    First commit for this project with skeleton project body from SpringInitializr

---
+++ src/main/java/com/safetynet/apiSafetyNet/ApiSafetyNetApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.apiSafetyNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSafetyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSafetyNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/apiSafetyNet/ApiSafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.apiSafetyNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
